6ba139908e088a6e6982be7d72da2ca29872713b
Dave Moten <davidmoten@gmail.com>
2018-03-01 16:50:31 +1100

    add test

---
+++ src/main/java/org/davidmoten/eq/UnexpectedException.java
@@ -0,0 +1,7 @@
+package org.davidmoten.eq;
+
+public final class UnexpectedException extends RuntimeException {
+
+    private static final long serialVersionUID = 2810786758593878301L;
+
+}
---
+++ src/test/java/org/davidmoten/eq/EmbeddedQueueTest.java
@@ -0,0 +1,25 @@
+package org.davidmoten.eq;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Test;
+
+public class EmbeddedQueueTest {
+
+    @Test
+    public void test() throws IOException {
+        Path directory = Files.createTempDirectory(new File("target").toPath(), "test");
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (EmbeddedQueue q = new EmbeddedQueue(directory.toFile(), 100, 3, "test")) {
+            q.addReader(0, out);
+            q.add(0, "boo".getBytes());
+            q.add(1, "you".getBytes());
+        }
+        System.out.println(out.toString());
+    }
+
+}
