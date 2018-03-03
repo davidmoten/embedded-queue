a271aabe910cf13a6fc74a15f2edfd4f515e26c2
Dave Moten <davidmoten@gmail.com>
2018-03-03 17:10:39 +1100

    refactor test

--- src/test/java/org/davidmoten/eq/EmbeddedQueueTest.java
+++ src/test/java/org/davidmoten/eq/EmbeddedQueueTest.java
@@ -11,28 +11,24 @@ import java.nio.file.Path;
 
 import org.davidmoten.eq.EmbeddedQueue.Reader;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EmbeddedQueueTest {
-
-    private static final Logger log = LoggerFactory.getLogger(EmbeddedQueueTest.class);
 
     @Test
     public void test() throws IOException, InterruptedException {
         Path directory = Files.createTempDirectory(new File("target").toPath(), "test");
-        SafeOutputStream out = new SafeOutputStream();
+        SynchronizedOutputStream out = new SynchronizedOutputStream();
         EmbeddedQueue q = new EmbeddedQueue(directory.toFile(), 100, 30000, 2);
         Reader reader = q.addReader(0, out);
         reader.start();
         q.addMessage(0, "boo".getBytes());
         q.addMessage(1, "you".getBytes());
         reader.request(5);
-        Thread.sleep(1000);
+        Thread.sleep(500);
         assertEquals(14, out.bytes().length);
     }
 
-    private static final class SafeOutputStream extends OutputStream {
+    private static final class SynchronizedOutputStream extends OutputStream {
 
         private final ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 
