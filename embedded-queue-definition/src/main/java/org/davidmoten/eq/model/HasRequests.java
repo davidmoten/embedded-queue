c0d1af405c4c8ad3147470acb5a5a6a5a56ff56c
Dave Moten <davidmoten@gmail.com>
2018-03-15 17:03:44 +1100

    state machine dev

---
+++ embedded-queue-definition/src/main/java/org/davidmoten/eq/model/HasRequests.java
@@ -0,0 +1,7 @@
+package org.davidmoten.eq.model;
+
+import com.github.davidmoten.fsm.runtime.Event;
+
+public class HasRequests implements Event<Reader> {
+
+}
---
+++ embedded-queue-definition/src/main/java/org/davidmoten/eq/model/NextSegment.java
@@ -0,0 +1,7 @@
+package org.davidmoten.eq.model;
+
+import com.github.davidmoten.fsm.runtime.Event;
+
+public class NextSegment implements Event<Reader>{
+
+}
