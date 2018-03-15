73c4a8b34ed5626647184f49e8093d19caf9dfa2
Dave Moten <davidmoten@gmail.com>
2018-03-15 17:03:26 +1100

    state machine dev

--- embedded-queue-definition/src/main/java/org/davidmoten/eq/StateMachineDefinitions.java
+++ embedded-queue-definition/src/main/java/org/davidmoten/eq/StateMachineDefinitions.java
@@ -6,12 +6,15 @@ import java.util.function.Supplier;
 
 import org.davidmoten.eq.model.EndOfFile;
 import org.davidmoten.eq.model.FirstSegment;
+import org.davidmoten.eq.model.HasRequests;
 import org.davidmoten.eq.model.LatestWasRead;
+import org.davidmoten.eq.model.NextSegment;
 import org.davidmoten.eq.model.OpenFile;
 import org.davidmoten.eq.model.Read;
 import org.davidmoten.eq.model.Reader;
 import org.davidmoten.eq.model.ReaderAdded;
 import org.davidmoten.eq.model.Request;
+import org.davidmoten.eq.model.RequestNextSegment;
 import org.davidmoten.eq.model.RequestsMet;
 import org.davidmoten.eq.model.Written;
 
@@ -33,7 +36,7 @@ public final class StateMachineDefinitions implements Supplier<List<StateMachine
                 .documentation("<pre>entry/\n" //
                         + "signal FirstSegmentRequest(reader: self, startingFrom: reader.offset) to store;\n"
                         + "</pre>");
-        State<Reader, FirstSegment> hasSegment = m.createState("Has Segment") //
+        State<Reader, FirstSegment> hasFirstSegment = m.createState("Has First Segment") //
                 .event(FirstSegment.class) //
                 .documentation("<pre>entry/\n" //
                         + "signal OpenFile to self");
@@ -52,14 +55,17 @@ public final class StateMachineDefinitions implements Supplier<List<StateMachine
                         + "</pre>");
         State<Reader, Request> requestedNoneAvailable = m.createState("Requested None Available") //
                 .event(Request.class);
-        State<Reader, Written> moreAvailableNoRequests = m.createState("More Available, No Requests").event(Written.class);
+        State<Reader, Written> moreAvailableNoRequests = m.createState("More Available, No Requests")
+                .event(Written.class);
         State<Reader, Request> moreAvailableRequested = m.createState("More Available, Requested").event(Request.class);
         State<Reader, Written> requestedMoreAvailable = m.createState("Requested, More Available").event(Written.class);
         State<Reader, LatestWasRead> latestRead = m.createState("Latest read, Requested").event(LatestWasRead.class);
+        State<Reader, RequestNextSegment> requestedNextSegment = m.createState("Requested Next Segment").event(RequestNextSegment.class);
+        State<Reader, NextSegment> hasNextSegment = m.createState("Has Next Segment").event(NextSegment.class);
 
         created //
                 .initial() //
-                .to(hasSegment) //
+                .to(hasFirstSegment) //
                 .to(fileOpened) //
                 .to(metRequests); //
         reading.to(closedFile);
@@ -72,6 +78,8 @@ public final class StateMachineDefinitions implements Supplier<List<StateMachine
         requestedMoreAvailable.to(reading);
         latestRead.from(reading);
         latestRead.to(requestedMoreAvailable);
+        closedFile.to(requestedNextSegment);
+        requestedNextSegment.to(hasNextSegment);
         return m;
     }
 
